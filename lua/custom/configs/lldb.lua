7f10d616f3c391ef438c7c04d5e5dcce1479b5f9
yaohuiming <yaohuiming789@gmail.com>
2023-07-20 01:51:00 +0800

    update

---
+++ lua/custom/configs/lldb.lua
@@ -0,0 +1,17 @@
+local mason_registry = require("mason-registry")
+
+local codelldb_root = mason_registry.get_package("codelldb"):get_install_path() .. "/extension/"
+local codelldb_path = codelldb_root .. "adapter/codelldb"
+local liblldb_path = codelldb_root .. "lldb/lib/liblldb.so"
+
+
+local opts = {
+    -- ... other configs
+    dap = {
+        adapter = require('rust-tools.dap').get_codelldb_adapter(
+            codelldb_path, liblldb_path)
+    }
+}
+
+require('rust-tools').setup(opts)
+
--- lua/custom/configs/rust-tools.lua
+++ lua/custom/configs/rust-tools.lua
@@ -1,12 +1,22 @@
 local on_attach = require("plugins.configs.lspconfig").on_attach
 local capabilities = require("plugins.configs.lspconfig").capabilities
 
+local mason_registry = require("mason-registry")
 
-local options = {
+local codelldb_root = mason_registry.get_package("codelldb"):get_install_path() .. "/extension/"
+local codelldb_path = codelldb_root .. "adapter/codelldb"
+-- 这里可能因为操作系统不同/插件版本不同后缀会改变, 如果debug出现问题是把后缀换成.so试试
+local liblldb_path = codelldb_root .. "lldb/lib/liblldb.dylib"
+
+local opts = {
   server = {
+    checkOnSave = {
     on_attach = on_attach,
     capabilities = capabilities,
-  }
+      allFeatures = true,
+    },
+  },
+  dap = { adapter = require("rust-tools.dap").get_codelldb_adapter(codelldb_path, liblldb_path) },
 }
 
-return options
+return opts
